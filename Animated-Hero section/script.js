4872f821cd94ac85e61eb3c13f99bbc1ebd405dd
SACH01in <sachinsingh11410129@gmail.com>
2025-08-04 17:30:01 +0530

    added two more section and scrollTrigger actions

--- Animated-Hero section/script.js
+++ Animated-Hero section/script.js
@@ -1,3 +1,4 @@
+document.body.style.backgroundColor = "#000";
 function HeroAnimation(){
     const tl = gsap.timeline({
         default:{
@@ -49,3 +50,96 @@ function HeroAnimation(){
 }
 
 HeroAnimation();
+
+// section 2
+document.querySelectorAll('section').forEach(elem => {
+  ScrollTrigger.create({
+    trigger: elem,
+    start: "top top",
+    end: "bottom top",
+    onEnter: () => {
+      document.body.style.backgroundColor = elem.dataset.background;
+    },
+    onEnterBack: () => {
+      document.body.style.backgroundColor = elem.dataset.background;
+    }
+  });
+});
+
+
+
+const tl2 = gsap.timeline();
+tl2.fromTo('.ssp',{
+    y:-300,
+    opacity:0,
+    scale:0,
+},{
+    y:0,
+    opacity: 1,
+    scale:15,
+    duration:2,
+    scrollTrigger:{
+        trigger: '.two',
+        start: "top 50%",
+        end: "bottom bottom",
+        scrub:2,
+        onEnterBack: () => {
+            gsap.to('.ssp',{
+            display:"block"
+        })
+        }
+    },
+    onComplete: () => {
+        gsap.to('.ssp',{
+            display:"none"
+        })
+    },
+    
+})
+
+const tl3 = gsap.timeline({
+    scrollTrigger:{
+        trigger:".three",
+        start: 'top top',
+        end: "bottom top",
+        scrub:2,
+        pin:true,
+    }
+})
+
+tl3.from('.left',{
+    x:-500,
+    opacity:0,
+    duration:2,
+},'wrapper')
+tl3.from('.right',{
+    x:500,
+    opacity:0,
+    duration:2,
+},'wrapper')
+
+gsap.from('.four p',{
+    y:100,
+    filter:"blur(5px)",
+    opacity:0,
+    duration:2,
+    scrollTrigger:{
+        trigger:".four",
+        start:"top top",
+        end:"bottom bottom",
+        scrub:2,
+        pin:true,
+    }
+})
+
+gsap.from('.four h1',{
+    bottom: "-80%",
+    duration:1,
+    scrollTrigger:{
+        trigger:".four",
+        start:"top top",
+        end:"bottom bottom",
+        scrub:2,
+        pin:true,
+    }
+})

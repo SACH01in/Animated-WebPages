f345d64839332dee49938d45918b3481c6866817
SACH01in <sachinsingh11410129@gmail.com>
2025-08-13 22:18:11 +0530

    Added Perloader and started work to make a complete webpage from second section

--- Animated-Hero section/script.js
+++ Animated-Hero section/script.js
@@ -1,3 +1,76 @@
+const lenis = new Lenis();
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+lenis.on('scroll', ScrollTrigger.update);
+
+function LoaderAnimation(){
+    function loaderLogoAnimation(){
+        const LoaderTl = gsap.timeline()
+        
+        LoaderTl.to('.loader .logo',{
+            width:200,
+            duration:1,
+        })
+        LoaderTl.to('.loader .logo h2 span',{
+            y:0,
+            duration:0.5,
+            stagger:0.1,
+        })
+        LoaderTl.to('.loader .logo h2 span',{
+            y:"100%",
+            duration:0.5,
+            stagger:0.1,
+        })
+        
+        LoaderTl.to('.loader .logo',{
+            width:0,
+            duration:1,
+        })
+        
+    }
+    loaderLogoAnimation();
+    const logoanimate = setInterval(() => {
+        loaderLogoAnimation()
+    },4000);
+
+    let loadVal = 0;
+    const counter = setInterval(() => {
+        if(loadVal < 100){
+            loadVal++;
+            document.querySelector('#loadVal').innerText = loadVal;
+        }
+        if(loadVal >= 100 ){
+            clearInterval(counter);
+            clearInterval(logoanimate);
+            gsap.set('.loader .logo',{ width: 200});
+            gsap.set('.loader .logo h2 span',{ y:0});
+            gsap.to(".loader", {
+                y: "-100vh",
+                duration: 1,
+                delay: 0.5,
+                // oncomplete: () => {
+                //     gsap.to('.loader', {
+                //         opacity:0,
+                //         duration:0.5
+                //     });
+                // }
+            });
+            
+        }
+    },40);
+    
+}
+LoaderAnimation();
+
+
+
 document.body.style.backgroundColor = "#000";
 function HeroAnimation(){
     const tl = gsap.timeline({
@@ -6,16 +79,25 @@ function HeroAnimation(){
             ease: "power4.easeInOut",
         }
     });
-
-    tl.from('.logo h2',{
+    tl.from('.one',{
+        y:"-100%",
+        duration:1,
+        delay:4,
+    });
+    tl.fromTo('#navbar .logo h2',{
         y:-100,
         opacity:0,
+    },{
+        y:0,
+        opacity:1,
+        stagger: 0.2,
     });
-
     tl.fromTo('.navlink a',{
         y:-100,
+        opacity:0,
     }, {
         y:0,
+        opacity:1,
         stagger: 0.2,
     });
 
@@ -46,100 +128,9 @@ function HeroAnimation(){
         opacity:1,
         delay:0.3,
         stagger:0.2,
-    },"heroBottom")
+    },"heroBottom");
+    tl.to('.loader',{
+        display:"none",
+    })
 }
-
 HeroAnimation();
-
-// section 2
-document.querySelectorAll('section').forEach(elem => {
-  ScrollTrigger.create({
-    trigger: elem,
-    start: "top top",
-    end: "bottom top",
-    onEnter: () => {
-      document.body.style.backgroundColor = elem.dataset.background;
-    },
-    onEnterBack: () => {
-      document.body.style.backgroundColor = elem.dataset.background;
-    }
-  });
-});
-
-
-
-const tl2 = gsap.timeline();
-tl2.fromTo('.ssp',{
-    y:-300,
-    opacity:0,
-    scale:0,
-},{
-    y:0,
-    opacity: 1,
-    scale:15,
-    duration:2,
-    scrollTrigger:{
-        trigger: '.two',
-        start: "top 50%",
-        end: "bottom bottom",
-        scrub:2,
-        onEnterBack: () => {
-            gsap.to('.ssp',{
-            display:"block"
-        })
-        }
-    },
-    onComplete: () => {
-        gsap.to('.ssp',{
-            display:"none"
-        })
-    },
-    
-})
-
-const tl3 = gsap.timeline({
-    scrollTrigger:{
-        trigger:".three",
-        start: 'top top',
-        end: "bottom top",
-        scrub:2,
-        pin:true,
-    }
-})
-
-tl3.from('.left',{
-    x:-500,
-    opacity:0,
-    duration:2,
-},'wrapper')
-tl3.from('.right',{
-    x:500,
-    opacity:0,
-    duration:2,
-},'wrapper')
-
-gsap.from('.four p',{
-    y:100,
-    filter:"blur(5px)",
-    opacity:0,
-    duration:2,
-    scrollTrigger:{
-        trigger:".four",
-        start:"top top",
-        end:"bottom bottom",
-        scrub:2,
-        pin:true,
-    }
-})
-
-gsap.from('.four h1',{
-    bottom: "-80%",
-    duration:1,
-    scrollTrigger:{
-        trigger:".four",
-        start:"top top",
-        end:"bottom bottom",
-        scrub:2,
-        pin:true,
-    }
-})
